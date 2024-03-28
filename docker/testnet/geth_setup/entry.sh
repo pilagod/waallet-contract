8b9bed6055d5ff0103ff6ad457069beb47663a4e
ChiHaoLu <b08303113@ntu.edu.tw>
2024-03-28 17:26:57 +0800

    Add imAccount init with WebAuthnValidator

--- docker/testnet/geth_setup/entry.sh
+++ docker/testnet/geth_setup/entry.sh
@@ -13,6 +13,7 @@ operator_private_key="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7b
 passkey_credential_id=${PASSKEY_CREDENTIAL_ID:-"9h5F3DgLSjSMdnVOadmhCw"}
 passkey_x=${PASSKEY_X:-67299174900712686363169673082376821529726602378544032702281553676098545184711}
 passkey_y=${PASSKEY_Y:-104273800132786176334597151467609377740095818152192999025225464410568038480397}
+authenticator_rpid_hash=${AUTHENTICATOR_RPID_HASH:-"0x507c5c427c4e8f53fd35937b4d6c16c79b8687979d428b91b8bd3ca91396afc3"}
 
 entry_point_address="0x5FbDB2315678afecb367f032d93F642f64180aa3"
 simple_account_factory_address="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
@@ -20,9 +21,11 @@ passkey_account_factory_address="0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
 verifying_paymaster_address="0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6"
 imAccount_implementation_address="0x610178dA211FEF7D417bC0e6FeD39F05609AD788"
 ecdsa_validator_address="0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e"
+webauthn_validator_address="0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE"
 fallback_handler_address="0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0"
 imAccount_factory_address="0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82"
 imAccount_salt=0
+imAccount_salt_webauthn=1
 
 # Deploy EntryPoint
 echo -e "\033[0;33m[Deploy EntryPoint]\033[0m"
@@ -95,13 +98,13 @@ echo -e "\033[0;33m[Setup Implementation Address]\033[0m"
 cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${imAccount_factory_address} "setImpl(address implAddr)" ${imAccount_implementation_address}
 
 # Deploy imAccountProxy (Create a new wallet)
-echo -e "\033[0;33m[Create imAccount]\033[0m"
+echo -e "\033[0;33m[Create imAccount w/ ECDSAValidator]\033[0m"
 validator_initializer=$(cast calldata "init(address owner)" ${operator_address})
 initializer=$(cast calldata "initialize(address,address,address,bytes calldata)" ${entry_point_address} ${fallback_handler_address} ${ecdsa_validator_address} ${validator_initializer})
 cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${imAccount_factory_address} "createAccount(bytes memory initializer, uint256 salt)" ${initializer} ${imAccount_salt}
 
 # Get imAccountProxy address
-echo -e "\033[0;33m[Get imAccount Address]\033[0m"
+echo -e "\033[0;33m[Get imAccount Address (w/ ECDSAValidator)]\033[0m"
 imAccount_address=$(cast call --rpc-url ${rpc_url} ${imAccount_factory_address} "getAddress(uint256 salt)" ${imAccount_salt} | sed -r 's/^[.]*(0x)([0]{24}|)([0-9a-zA-Z]{40})[.]*$/\1\3/g')
 echo ${imAccount_address}
 
@@ -129,3 +132,18 @@ cd p256-verifier
 git checkout 4287b1714c2457514c97f47f55ff830d310a60cb
 forge install
 forge script ./script/Deploy.s.sol:DeployScript --rpc-url ${rpc_url} --broadcast --private-key ${operator_private_key}
+
+# Deploy imAccountProxy (Create a new wallet)
+echo -e "\033[0;33m[Create imAccount w/ WebAuthnValidator]\033[0m"
+validator_initializer=$(cast calldata "init(uint256 ownerX,uint256 ownerY,bytes32 authenticatorRPIDHash)" ${passkey_x} ${passkey_y} ${authenticator_rpid_hash})
+initializer=$(cast calldata "initialize(address,address,address,bytes calldata)" ${entry_point_address} ${fallback_handler_address} ${webauthn_validator_address} ${validator_initializer})
+cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${imAccount_factory_address} "createAccount(bytes memory initializer, uint256 salt)" ${initializer} ${imAccount_salt_webauthn}
+
+# Get imAccountProxy address
+echo -e "\033[0;33m[Get imAccount Address (w/ WebAuthnValidator)]\033[0m"
+imAccount_address=$(cast call --rpc-url ${rpc_url} ${imAccount_factory_address} "getAddress(uint256 salt)" ${imAccount_salt_webauthn} | sed -r 's/^[.]*(0x)([0]{24}|)([0-9a-zA-Z]{40})[.]*$/\1\3/g')
+echo ${imAccount_address}
+
+# Topup imAccountProxy
+echo -e "\033[0;33m[Transfer 100 ETH to imAccount]\033[0m"
+cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${imAccount_address} --value 100ether
