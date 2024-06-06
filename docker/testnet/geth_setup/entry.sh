42723d2f6bab9098dfa066c4d9c0969e290bf08d
Irara <irara.chen@token.im>
2024-06-06 15:00:15 +0800

    chg: Adjust the order of comments and ETH topup

--- docker/testnet/geth_setup/entry.sh
+++ docker/testnet/geth_setup/entry.sh
@@ -11,15 +11,18 @@ curl -d '{"id":1,"jsonrpc":"2.0","method":"clique_propose","params":["0xf39Fd6e5
 operator_address="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 operator_private_key="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 
-# For deploying Counter and other contracts
+# For deploying periphery contracts
 deployer_1_address="0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
 deployer_1_private_key="0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
-# For deploying account abstraction related contracts v0.6
+cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${deployer_1_address} --value 100ether
+# For deploying account abstraction v0.6.0 contracts
 deployer_2_address="0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"
 deployer_2_private_key="0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"
-# For deploying account abstraction related contracts v0.7
+cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${deployer_2_address} --value 100ether
+# For deploying account abstraction v0.7.0 contracts
 deployer_3_address="0x90F79bf6EB2c4f870365E785982E1f101E93b906"
 deployer_3_private_key="0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6"
+cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${deployer_3_address} --value 100ether
 
 passkey_credential_id=${PASSKEY_CREDENTIAL_ID:-"9h5F3DgLSjSMdnVOadmhCw"}
 passkey_x=${PASSKEY_X:-67299174900712686363169673082376821529726602378544032702281553676098545184711}
@@ -29,11 +32,6 @@ entry_point_address="0x663F3ad617193148711d28f5334eE4Ed07016602"
 simple_account_factory_address="0x2E983A1Ba5e8b38AAAeC4B440B9dDcFBf72E15d1"
 passkey_account_factory_address="0xBC9129Dc0487fc2E169941C75aABC539f208fb01"
 verifying_paymaster_address="0xF6168876932289D073567f347121A267095f3DD6"
-
-# Topup deployers
-cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${deployer_1_address} --value 100ether
-cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${deployer_2_address} --value 100ether
-cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${deployer_3_address} --value 100ether
 
 # Deploy non-account abstraction contracts
 echo -e "\033[0;33m[Deploy non-account abstraction contracts]\033[0m"
