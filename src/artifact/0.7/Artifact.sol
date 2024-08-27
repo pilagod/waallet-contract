2f91d7261e18877b4e8b0c479d86266ae94a0de9
pilagod <pilagooood@gmail.com>
2024-08-27 11:37:42 +0800

    add token paymaster to artifact

--- src/artifact/0.7/Artifact.sol
+++ src/artifact/0.7/Artifact.sol
@@ -5,5 +5,7 @@ import { EntryPoint } from
     "@account-abstraction/0.7/contracts/core/EntryPoint.sol";
 import { SimpleAccountFactory } from
     "@account-abstraction/0.7/contracts/samples/SimpleAccountFactory.sol";
+import { TokenPaymaster } from
+    "@account-abstraction/0.7/contracts/samples/TokenPaymaster.sol";
 import { VerifyingPaymaster } from
     "@account-abstraction/0.7/contracts/samples/VerifyingPaymaster.sol";
