762ec2e54281dc6ddb1b27f6728614a5945eb525
pilagod <pilagooood@gmail.com>
2024-08-27 14:36:38 +0800

    deploy token paymaster v0.7 to testnet

--- docker/testnet/geth_setup/entry.sh
+++ docker/testnet/geth_setup/entry.sh
@@ -27,9 +27,11 @@ cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${bundler_v
 # For deploying periphery contracts
 deployer_1_address="0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
 deployer_1_private_key="0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
+
 # For deploying account abstraction v0.6.0 contracts
 deployer_2_address="0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"
 deployer_2_private_key="0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"
+
 # For deploying account abstraction v0.7.0 contracts
 deployer_3_address="0x90F79bf6EB2c4f870365E785982E1f101E93b906"
 deployer_3_private_key="0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6"
@@ -54,6 +56,9 @@ echo -e "\033[0;33m[Deploy periphery contracts]\033[0m"
 
 counter_address="0x8464135c8F25Da09e49BC8782676a84730C318bC"
 test_token_address="0x71C95911E9a5D330f4D621842EC243EE1343292e"
+weth_address="0x948B3c65b89DF0B4894ABE91E6D02FE579834F8F"
+mock_oracle_address="0x712516e61C8B383dF4A63CFe83d7701Bce54B03e"
+mock_uniswap_v3_swap_router_address="0xbCF26943C0197d2eE0E5D05c716Be60cc2761508"
 
 # Deploy Counter
 echo -e "\033[0;33m[Deploy Counter]\033[0m"
@@ -62,6 +67,23 @@ forge create --rpc-url ${rpc_url} --private-key ${deployer_1_private_key} src/Co
 # Deploy Test Token
 echo -e "\033[0;33m[Deploy Test Token]\033[0m"
 forge create --rpc-url ${rpc_url} --private-key ${deployer_1_private_key} src/token/ERC20Mintable.sol:ERC20Mintable --constructor-args "Test Token" "TEST"
+
+# Deploy WETH
+echo -e "\033[0;33m[Deploy WETH]\033[0m"
+forge create --rpc-url ${rpc_url} --private-key ${deployer_1_private_key} src/token/WETH.sol:WETH
+
+# Deploy Mock Oracle
+echo -e "\033[0;33m[Deploy Mock Oracle]\033[0m"
+forge create --rpc-url ${rpc_url} --private-key ${deployer_1_private_key} src/mock/MockOracle.sol:MockOracle --constructor-args ${test_token_address}
+
+# Deploy Mock Uniswap v3 Swap Router
+echo -e "\033[0;33m[Deploy Mock Uniswap v3 Swap Router]\033[0m"
+forge create --rpc-url ${rpc_url} --private-key ${deployer_1_private_key} src/mock/MockUniswapV3SwapRouter.sol:MockUniswapV3SwapRouter --constructor-args ${weth_address}
+
+# Deposit WETH to Mock Uniswap v3 Swap Router
+echo -e "\033[0;33m[Deposit 100 WETH to Mock Uniswap v3 Swap Router]\033[0m"
+cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${weth_address} --value 100ether
+cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${weth_address} "transfer(address dst, uint256 wad)" ${mock_uniswap_v3_swap_router_address} 100ether
 
 ###############################################
 # Deploy account abstraction v0.6.0 contracts #
@@ -139,6 +161,7 @@ simple_account_factory_address_v0_7="0x261D8c5e9742e6f7f1076Fa1F560894524e19cad"
 simple_account_address_v0_7="0xD0dA07666BA2139aa6fF7A450A8596291a6cE471"
 passkey_account_factory_address_v0_7="0xCba6b9A951749B8735C603e7fFC5151849248772"
 verifying_paymaster_address_v0_7="0xcf27F781841484d5CF7e155b44954D7224caF1dD"
+token_paymaster_address_v0_7="0x673cD70FA883394a1f3DEb3221937Ceb7C2618D7"
 
 # Deploy EntryPoint v0.7
 echo -e "\033[0;33m[Deploy EntryPoint v0.7]\033[0m"
@@ -192,3 +215,11 @@ forge create --rpc-url ${rpc_url} --private-key ${deployer_3_private_key} lib/ac
 # Deposit to EntryPoint for VerifyingPaymaster v0.7
 echo -e "\033[0;33m[Deposit 100 ETH to EntryPoint for VerifyingPaymaster v0.7]\033[0m"
 cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${entry_point_address_v0_7} --value 100ether "depositTo(address account)" ${verifying_paymaster_address_v0_7}
+
+# Deploy TokenPaymaster v0.7
+echo -e "\033[0;33m[Deploy TokenPaymaster v0.7]\033[0m"
+forge create --rpc-url ${rpc_url} --private-key ${deployer_3_private_key} lib/account-abstraction/0.7/contracts/samples/TokenPaymaster.sol:TokenPaymaster --constructor-args ${test_token_address} ${entry_point_address_v0_7} ${weth_address} ${mock_uniswap_v3_swap_router_address} "($(echo "1.1 * 10^26" | bc | cut -d'.' -f1),$((100*10**18)),100000,86400)" "(0,100,${mock_oracle_address},${zero_address},true,false,false,0)" "(1,0,0)" ${operator_address}
+
+# Deposit to EntryPoint for VerifyingPaymaster v0.7
+echo -e "\033[0;33m[Deposit 100 ETH to EntryPoint for TokenPaymaster v0.7]\033[0m"
+cast send --rpc-url ${rpc_url} --private-key ${operator_private_key} ${entry_point_address_v0_7} --value 100ether "depositTo(address account)" ${token_paymaster_address_v0_7}
